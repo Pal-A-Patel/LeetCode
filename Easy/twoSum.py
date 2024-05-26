5f3334ff7e55488bd3e987983805ddaf9cea4ded
Pal-A-Patel <134096532+Pal-A-Patel@users.noreply.github.com>
2024-05-26 11:36:33 -0600

    Update twoSum.py

--- Easy/twoSum.py
+++ Easy/twoSum.py
@@ -1,1 +1,16 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        m = {}
+        for i, x in enumerate(nums):
+            print(x)
+            y = target - x
+            if y in m:
+                return [m[y], i]
+            m[x] = i
 
+          
